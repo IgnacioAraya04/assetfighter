b7e3d5975f4dd5094279efd6404ff7a107168145
IgnacioAraya04 <167039165+IgnacioAraya04@users.noreply.github.com>
2024-05-10 13:41:03 -0400

    ambos pj pueden atacar

--- adventurer.gd
+++ adventurer.gd
@@ -6,6 +6,7 @@ const JUMP_VELOCITY = -600.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var isAtacking = false
 var lePegan = false
 
 func _physics_process(delta):
@@ -14,17 +15,19 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction == +1 and lePegan == false:
+	if direction == +1 and lePegan == false and isAtacking == false:
 		velocity.x = direction * SPEED
 		$AnimatedSprite2D.flip_h = false
+		get_node("areaataque").set_scale(Vector2(1, 1))
 		$AnimatedSprite2D.play("run")
-	elif direction == -1 and lePegan == false:
+	elif direction == -1 and lePegan == false and isAtacking == false:
 		velocity.x = direction * SPEED
 		$AnimatedSprite2D.flip_h = true
+		get_node("areaataque").set_scale(Vector2(-1, 1))
 		$AnimatedSprite2D.play("run")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		if lePegan == false:
+		if lePegan == false and isAtacking == false:
 			$AnimatedSprite2D.play("idle")
 		
 	if not is_on_floor():
@@ -34,17 +37,25 @@ func _physics_process(delta):
 			$AnimatedSprite2D.play("jump")
 		
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_up") and is_on_floor() and lePegan == false:
+	if Input.is_action_just_pressed("ui_up") and is_on_floor() and lePegan == false and isAtacking == false:
 		$AnimatedSprite2D.play("jump")
 		velocity.y = JUMP_VELOCITY
 		
-	move_and_slide()
+	if Input.is_action_just_pressed("attackp2") and is_on_floor():
+		isAtacking= true
+		$areaataque/colisionataque.disabled = false
+		$AnimatedSprite2D.play("ataque")
+		
+	move_and_slide()	
 
 func _on_areadaño_area_entered(area):
-		if area.is_in_group("espada"):
+		if area.is_in_group("ataque"):
 			$AnimatedSprite2D.play("hurt")
 			lePegan= true
 
 func _on_animated_sprite_2d_animation_finished():
 	if $AnimatedSprite2D.animation == "hurt":
 		lePegan = false
+	if $AnimatedSprite2D.animation == "ataque":
+		$areaataque/colisionataque.disabled = true
+		isAtacking = false
--- knight.gd
+++ knight.gd
@@ -6,26 +6,27 @@ const JUMP_VELOCITY = -600.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var isAtacking= false;
+var isAtacking= false
+var lePegan = false
 
 func _physics_process(delta):
 	# Add the gravity
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("left", "right")
-	if direction == +1 and isAtacking == false:
+	if direction == +1 and isAtacking == false and lePegan == false:
 		velocity.x = direction * SPEED
 		$AnimatedSprite2D.flip_h = false
 		get_node("areaataque").set_scale(Vector2(1, 1))
 		$AnimatedSprite2D.play("run")
-	elif direction == -1 and isAtacking == false:
+	elif direction == -1 and isAtacking == false and lePegan == false:
 		velocity.x = direction * SPEED
 		$AnimatedSprite2D.flip_h = true
 		get_node("areaataque").set_scale(Vector2(-1, 1))
 		$AnimatedSprite2D.play("run")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		if isAtacking == false:
+		if isAtacking == false and lePegan == false:
 			$AnimatedSprite2D.play("idle")
 		
 	if not is_on_floor():
@@ -35,11 +36,11 @@ func _physics_process(delta):
 			$AnimatedSprite2D.play("jump")
 		
 	# Handle jump.
-	if Input.is_action_just_pressed("up") and is_on_floor():
+	if Input.is_action_just_pressed("up") and is_on_floor() and isAtacking ==false and lePegan == false:
 		$AnimatedSprite2D.play("jump")
 		velocity.y = JUMP_VELOCITY
 	
-	if Input.is_action_just_pressed("attack"):
+	if Input.is_action_just_pressed("attackp1") and is_on_floor():
 		isAtacking= true
 		$areaataque/colisionataque.disabled = false
 		$AnimatedSprite2D.play("espadazo")
@@ -51,3 +52,11 @@ func _on_animated_sprite_2d_animation_finished():
 	if $AnimatedSprite2D.animation == "espadazo":
 		$areaataque/colisionataque.disabled = true
 		isAtacking = false	
+	if$AnimatedSprite2D.animation == "hurt":
+		lePegan = false
+
+
+func _on_areadaño_area_entered(area):
+	if area.is_in_group("ataquep2"):
+		$AnimatedSprite2D.play("hurt")
+		lePegan = true
