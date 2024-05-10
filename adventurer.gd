776d521275c20e923f5f968d5235c55b9df19f4f
IgnacioAraya04 <167039165+IgnacioAraya04@users.noreply.github.com>
2024-05-10 13:07:32 -0400

    animacion_ataque(caballero),animacion_daño(elotro)

--- adventurer.gd
+++ adventurer.gd
@@ -6,7 +6,7 @@ const JUMP_VELOCITY = -600.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var lePegan = false
 
 func _physics_process(delta):
 	# Add the gravity
@@ -14,17 +14,18 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction == +1:
+	if direction == +1 and lePegan == false:
 		velocity.x = direction * SPEED
 		$AnimatedSprite2D.flip_h = false
 		$AnimatedSprite2D.play("run")
-	elif direction == -1:
+	elif direction == -1 and lePegan == false:
 		velocity.x = direction * SPEED
 		$AnimatedSprite2D.flip_h = true
 		$AnimatedSprite2D.play("run")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		$AnimatedSprite2D.play("idle")
+		if lePegan == false:
+			$AnimatedSprite2D.play("idle")
 		
 	if not is_on_floor():
 		$AnimatedSprite2D.play("fall")
@@ -33,8 +34,17 @@ func _physics_process(delta):
 			$AnimatedSprite2D.play("jump")
 		
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+	if Input.is_action_just_pressed("ui_up") and is_on_floor() and lePegan == false:
 		$AnimatedSprite2D.play("jump")
 		velocity.y = JUMP_VELOCITY
 		
 	move_and_slide()
+
+func _on_areadaño_area_entered(area):
+		if area.is_in_group("espada"):
+			$AnimatedSprite2D.play("hurt")
+			lePegan= true
+
+func _on_animated_sprite_2d_animation_finished():
+	if $AnimatedSprite2D.animation == "hurt":
+		lePegan = false
--- knight.gd
+++ knight.gd
@@ -6,25 +6,27 @@ const JUMP_VELOCITY = -600.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var isAtacking= false;
 
 func _physics_process(delta):
 	# Add the gravity
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("left", "right")
-	if direction == +1:
+	if direction == +1 and isAtacking == false:
 		velocity.x = direction * SPEED
 		$AnimatedSprite2D.flip_h = false
+		get_node("areaataque").set_scale(Vector2(1, 1))
 		$AnimatedSprite2D.play("run")
-	elif direction == -1:
+	elif direction == -1 and isAtacking == false:
 		velocity.x = direction * SPEED
 		$AnimatedSprite2D.flip_h = true
+		get_node("areaataque").set_scale(Vector2(-1, 1))
 		$AnimatedSprite2D.play("run")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		$AnimatedSprite2D.play("idle")
+		if isAtacking == false:
+			$AnimatedSprite2D.play("idle")
 		
 	if not is_on_floor():
 		$AnimatedSprite2D.play("fall")
@@ -36,5 +38,16 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("up") and is_on_floor():
 		$AnimatedSprite2D.play("jump")
 		velocity.y = JUMP_VELOCITY
+	
+	if Input.is_action_just_pressed("attack"):
+		isAtacking= true
+		$areaataque/colisionataque.disabled = false
+		$AnimatedSprite2D.play("espadazo")
 		
 	move_and_slide()
+
+
+func _on_animated_sprite_2d_animation_finished():
+	if $AnimatedSprite2D.animation == "espadazo":
+		$areaataque/colisionataque.disabled = true
+		isAtacking = false	
