7f59c7714174339678e77d9332e4dc2f6f8ef722
manuel <168233473+Kaz2515@users.noreply.github.com>
2024-05-24 00:02:16 -0400

    vidas_imagenes

---
+++ Escenas/Vida P2.gd
@@ -0,0 +1,6 @@
+extends Control
+
+var tmaño_vida: int = 80
+
+func _on_knight_cambio_vida(vida_jugador):
+	
---
+++ Escenas/vida P1.gd
@@ -0,0 +1,6 @@
+extends Control
+
+
+
+func _on_knight_cambio_vida(vida_jugador):
+	pass # Replace with function body.
