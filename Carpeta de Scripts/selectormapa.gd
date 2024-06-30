8b0e8e00ec247e754efb33ddd6e43cde00d76b33
SebaVCH <109924179+SebaVCH@users.noreply.github.com>
2024-06-30 16:07:57 -0400

    Agregar mapas y modificar cosas.

--- Carpeta de Scripts/selectormapa.gd
+++ Carpeta de Scripts/selectormapa.gd
@@ -9,3 +9,11 @@ func _on_woods_pressed():
 
 func _on_plain_pressed():
 	get_tree().change_scene_to_file("res://Escenas/ModoSolo.tscn")
+
+
+func _on_battlefield_pressed():
+	get_tree().change_scene_to_file("res://Escenas/Battlefield.tscn")
+
+
+func _on_mapita_pressed():
+	get_tree().change_scene_to_file("res://Escenas/drawland.tscn")
